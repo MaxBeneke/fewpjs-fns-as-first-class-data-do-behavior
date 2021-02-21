b1116d8036fb42bd76f68ed60f4168db6156887b
Max Beneke <max.beneke@gmail.com>
2021-02-21 14:37:00 -0500

    commit tree 1c85c7585988daae74c8aa6d81eb0d6acee9935f

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
+function displayMessage(string) {
+  const arr = string.split(':');
+  const t = parseInt(arr[0]);
+  if (t < 12) {
+    console.log("Good Morning");
+  } else if (t >= 12 && t <= 17) {
+    console.log("Good Afternoon");
+  } else if (t > 17) {
+    console.log("Good Evening");
+  }
+}
+
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
